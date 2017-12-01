dede45170bdabecf90140cf3491f4a9140f95e41
Jim Reinknecht <captainjimscharters@gmail.com>
2017-12-01 10:30:58 -0500

    updated. forms need work

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ var sequelize = require('sequelize');
 var db = require('./models');
 var flash = require('connect-flash');
 
-// // Create an instance of the express app.
+// Create an instance of the express app.
 var app = express();
-//
 // // Specify the port.
 var port = process.env.PORT || '9000';
-//
-// // Set Handlebars as the default templating engine.
+//  Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({
     defaultLayout: "layout",
     layoutsDir: __dirname + "/views"
@@ -26,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+// init connect-flash for messages
 app.use(flash());
 
 //
@@ -50,46 +49,42 @@ app.get("/", (req, res, next) => {
 
 
 app.post("/", (req, res, next) => {
-
-    //var newBurger = req.body;
-    // connection.query("INSERT INTO ready SET ?", { burger_name: newBurger.burger_name, user_created: newBurger.user_created, is_eaten: 0 }, (err, result) => {
-    db.burger.create(req.body, {}).done(data => {
-        return res.redirect('/');
+    db.burger.create(req.body, {}).then(data => {
+        db.burger.findAll({ where: { is_eaten: false } }).then(notEaten => {
+            db.burger.findAll({ where: { is_eaten: true } }).then(isEaten => {
+                return res.render("burger", {
+                    title: "Boogers, Not Burgers v2.0",
+                    notEaten: notEaten,
+                    isEaten: isEaten,
+                    message: "Post Successful"
+                })
+            })
+        })
     })
-
-    //     if (err) throw err
-    //     res.redirect("/");
-    // });
-
-
 });
+
+
 app.put("/eatburger/:id", (req, res, next) => {
     var eatBurger = req.params.id;
     console.log(eatBurger);
     db.burger.update({ is_eaten: true }, {
-            where: { id: eatBurger }
-        }).done(results => {
-            console.log(results)
-            db.burger.findAll({ where: { is_eaten: true } }).then(isEaten => {
-                db.burger.findAll({ where: { is_eaten: true } }).then(notEaten => {
-                    return res.send({ notEaten: notEaten, isEaten: isEaten });
-
-                })
+        where: { id: eatBurger }
+    }).done(results => {
+        console.log(results)
+        db.burger.findAll({ where: { is_eaten: true } }).then(isEaten => {
+            db.burger.findAll({ where: { is_eaten: true } }).then(notEaten => {
+                return res.send({ notEaten: notEaten, isEaten: isEaten });
 
             })
 
         })
-        // connection.query("UPDATE ready SET is_eaten = ? WHERE id =" + eatBurger, { is_eaten: 1 }, (error, results) => {
-        //     console.log(results);
-        //     if (error) throw error;{}
-        //     return res.redirect("/");
 
-    // });
+    })
 });
 app.get("/:id", (req, res, next) => {
     var burger = req.params.id
-    db.burger.findOne({ where: { id: burger } }).then(stuff => {
-
+    db.burger.findOne({ where: { id: burger } }).then((stuff) => {
+        return res.json(stuff)
     }).catch(error => {
         return res.render("burger", {
             title: "Boogers, Not Burgers v2.0",
@@ -98,6 +93,19 @@ app.get("/:id", (req, res, next) => {
         })
     })
 });
+
+app.delete("/:id", (req, res, next) => {
+    var deleteBurger = req.params.id
+    if (deleteBurger) {
+
+    } else {
+        console.log("Error in delete!")
+        return res.send("Error!!")
+    }
+
+})
+
+
 // Initiate the listener.
 db.sequelize.sync({ force: false })
     .then(function() {
